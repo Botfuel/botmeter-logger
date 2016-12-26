82745559599ef2e20584069877b5993f4b80084a
Oscar Roche <oscar.w.roche@gmail.com>
2016-12-26 10:10:20 +0100

    fixed style

--- botmeter_logger.js
+++ botmeter_logger.js
@@ -2,20 +2,28 @@
 
 var request = require('request');
 
-function BotmeterLoggerBotbuilder (url) {
+var indexDocument = function (document, url, cb) {
+  var requestData = {
+    uri: url,
+    method: 'POST',
+    json: document
+  };
+  /*jslint unparam: true */
+  request(requestData, function (error, response, body) {
+    if (error) {
+      cb(error, null);
+    } else {
+      cb(null, body);
+    }
+  });
+  /*jslint unparam: false */
+};
+
+var BotmeterLoggerBotbuilder = function (url) {
   var that = this;
   that.incomingMessages = {};
 
-  that.receive = function (body, next) {
-    that.incomingMessages[body.address.id] = body.text;
-    next();
-  };
-
-  that.send = function (response, next) {
-    logDocument(that.incomingMessages[response.address.id],response,next);
-  };
-
-  function logDocument(body, response, next) {
+  that.logDocument = function (body, response, next) {
     var doc = {
       "bot_version": response.address.bot.id,
       "channel": response.address.channelId,
@@ -27,24 +35,33 @@ function BotmeterLoggerBotbuilder (url) {
       "responses": [response.text]
     };
     var messageId = response.address.id;
-    indexDocument(doc, url, function (e, d){
+    indexDocument(doc, url, function (e, d) {
       if (e) {
         console.log(e);
-        delete(that.incomingMessages[messageId]);
-      };
+        delete (that.incomingMessages[messageId]);
+      }
       if (d) {
         console.log(d);
-        delete(that.incomingMessages[messageId]);
-      };
+        delete (that.incomingMessages[messageId]);
+      }
     });
     next();
   };
-}
 
-function BotmeterLoggerFacebook(url) {
+  that.receive = function (body, next) {
+    that.incomingMessages[body.address.id] = body.text;
+    next();
+  };
+
+  that.send = function (response, next) {
+    that.logDocument(that.incomingMessages[response.address.id], response, next);
+  };
+};
+
+var BotmeterLoggerFacebook = function (url) {
   var that = this;
 
-  that.logDocument = function (body, response) {
+  that.logDocument = function (body, response, cb) {
     var doc = {
       "channel": "facebook",
       "timestamp": Date.now(),
@@ -53,35 +70,21 @@ function BotmeterLoggerFacebook(url) {
       "body_type": "text",
       "responses": [response.message.text]
     };
-    indexDocument(doc, url, function (e, d){
-    if (e) {
-      console.log(e);
-    };
-    if (d) {
-      console.log(d);
-    }
-  });  
-  }
-};
-
-function indexDocument (document, url, cb) {
-  const requestData = {
-    uri: url,
-    method: 'POST',
-    json: document
+    indexDocument(doc, url, function (e, d) {
+      if (e) {
+        cb(e, null);
+      } else {
+        cb(null, d);
+      }
+    });
   };
-  request(requestData , function (error, response, body) {
-    if(error) {
-      cb(error,null);
-    } else {
-      cb(null,body);
-    }
-  });
+
 };
 
-module.exports = function(url) {
+module.exports = function (url) {
   return {
     botbuilder: new BotmeterLoggerBotbuilder(url),
     facebook: new BotmeterLoggerFacebook(url)
-  }
+  };
 };
+
