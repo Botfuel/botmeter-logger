2b0a1d2642b6381e19f8e291c2c8661a6dd1180d
Kevin ADDA <kevin.adda@botfuel.io>
2017-09-22 10:31:32 +0200

    update(resource): clean parameters

--- src/botmeter_logger.js
+++ src/botmeter_logger.js
@@ -2,6 +2,22 @@ import request from 'request';
 import { BOTMETER_API } from './config';
 
 class BotmeterLogger {
+  /**
+   * Clean parameters object by filtering out undefineds.
+   *
+   * @param {Object} params the parameters object.
+   * @returns {Object} the clean object of parameters
+   * @static
+   * @memberof ApiResource
+   */
+  static cleanParameters = params =>
+    Object.keys(params).reduce((returns, element) => {
+      if (params[element] !== undefined) {
+        return { ...returns, [element]: params[element] };
+      }
+      return returns;
+    }, {});
+
   constructor({ appId, appKey }) {
     this.headers = {
       'App-Id': appId,
@@ -14,7 +30,7 @@ class BotmeterLogger {
       uri: BOTMETER_API,
       method: 'POST',
       headers: this.headers,
-      json: document,
+      json: this.constructor.cleanParameters(document),
     };
     request(requestData, (error, response, body) => {
       if (error) {
